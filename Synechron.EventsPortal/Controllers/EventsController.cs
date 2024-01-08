462bc2665fd627b1ee741a22886faeb1f133d149
Shashwat1112 <shashwatsharma111@gmail.com>
2024-01-08 21:27:56 +0530

    First commit

---
+++ Synechron.EventsPortal/Controllers/EventsController.cs
@@ -0,0 +1,55 @@
+﻿using Synechron.EventsPortal.Dal;
+using Synechron.EventsPortal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Synechron.EventsPortal.Controllers
+{
+    public class EventsController : Controller
+    {
+        private readonly EventsDal _eventsDal;
+        public EventsController(EventsDal dal)
+        {
+            _eventsDal = dal;
+        }
+        // GET: Employees
+        public ActionResult Index()
+        {
+            ViewBag.PageTitle = "Welcome To Synechron Events List!";
+            ViewBag.PageSubTitle = "Core Development Team Of Bharat!";
+            return View(_eventsDal.GetAllEvents());
+        }
+        public ActionResult Details(int id)
+        {
+            ViewBag.PageTitle = "Welcome To Synechron Events List!";
+            ViewBag.PageSubTitle = "Core Development Team Of Bharat!";
+            return View(_eventsDal.GetEventDetails(id));
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Event events)
+        {
+            if (ModelState.IsValid)
+            {
+                events.Logo= "~/Images/logo.svg";
+                int result = _eventsDal.InsertEvent(events);    
+                if (result > 0)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ Synechron.EventsPortal/Dal/EventsDal.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using Synechron.EventsPortal.Models;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+
+namespace Synechron.EventsPortal.Dal
+{
+    public class EventsDal
+    {
+        public List<Event> GetAllEvents() 
+        {
+            using (MySqlConnection CN = new MySqlConnection(ConfigurationManager.ConnectionStrings["SynechronEventsConStr"].ConnectionString))
+            {
+                using (MySqlCommand CMD = new MySqlCommand())
+                {
+                    CN.Open();
+                    CMD.Connection = CN;
+                    CMD.CommandType = CommandType.StoredProcedure;
+                    CMD.CommandText = "ShowAllEvents";
+                    MySqlDataReader DR = CMD.ExecuteReader();
+                    List<Event> events = new List<Event>();
+                    while (DR.Read())
+                    {
+                        events.Add(new Event()
+                        {
+                            EventId = Convert.ToInt32(DR["EventId"]),
+                            EventCode = Convert.ToString(DR["EventCode"]),
+                            EventName = Convert.ToString(DR["EventName"]),
+                            Description = Convert.ToString(DR["Description"]),
+                            StartDate = Convert.ToDateTime(DR["StartDate"]),
+                            EndDate = Convert.ToDateTime(DR["EndDate"]),
+                            Fees = Convert.ToDecimal(DR["Fees"]),
+                            TotalSeatsFilled = Convert.ToInt32(DR["TotalSeatsFilled"]),
+                            Logo = Convert.ToString(DR["Logo"]),
+                        });
+                    }
+                    DR.Close();
+                    CN.Close();
+                    return events;
+                }
+            }
+        }
+
+        public Event GetEventDetails(int eventId)
+        {
+            using (MySqlConnection CN = new MySqlConnection(ConfigurationManager.ConnectionStrings["SynechronEventsConStr"].ConnectionString))
+            {
+                using (MySqlCommand CMD = new MySqlCommand())
+                {
+                    CN.Open();
+                    CMD.Connection = CN;
+                    CMD.CommandType = CommandType.StoredProcedure;
+                    CMD.CommandText = "ShowEventDetails";
+                    CMD.Parameters.AddWithValue("p_EventId", eventId);
+                    MySqlDataReader DR = CMD.ExecuteReader();
+                    DR.Read();
+                    Event events = new Event()
+                    {
+                        EventId = Convert.ToInt32(DR["EventId"]),
+                        EventCode = Convert.ToString(DR["EventCode"]),
+                        EventName = Convert.ToString(DR["EventName"]),
+                        Description = Convert.ToString(DR["Description"]),
+                        StartDate = Convert.ToDateTime(DR["StartDate"]),
+                        EndDate = Convert.ToDateTime(DR["EndDate"]),
+                        Fees = Convert.ToDecimal(DR["Fees"]),
+                        TotalSeatsFilled = Convert.ToInt32(DR["TotalSeatsFilled"]),
+                        Logo = Convert.ToString(DR["Logo"])
+                    };
+
+                    DR.Close();
+                    CN.Close();
+                    return events;
+                }
+            }
+        }
+        public int InsertEvent(Event events)
+        {
+            using (MySqlConnection CN = new MySqlConnection(ConfigurationManager.ConnectionStrings["SynechronEventsConStr"].ConnectionString))
+            {
+                using (MySqlCommand CMD = new MySqlCommand())
+                {
+                    CN.Open();
+                    CMD.Connection = CN;
+                    CMD.CommandType = CommandType.StoredProcedure;
+                    CMD.CommandText = "InsertEvent";
+                    CMD.Parameters.AddWithValue("p_EventId", events.EventId);
+                    CMD.Parameters.AddWithValue("p_EventCode", events.EventCode);
+                    CMD.Parameters.AddWithValue("p_EventName", events.EventName);
+                    CMD.Parameters.AddWithValue("p_Description", events.Description);
+                    CMD.Parameters.AddWithValue("p_StartDate", events.StartDate);
+                    CMD.Parameters.AddWithValue("p_EndDate", events.EndDate);
+                    CMD.Parameters.AddWithValue("p_Fees", events.Fees);
+                    CMD.Parameters.AddWithValue("p_TotalSeatsFilled", events.TotalSeatsFilled);
+                    CMD.Parameters.AddWithValue("p_Logo", events.Logo);
+                    int result = CMD.ExecuteNonQuery();
+                    CN.Close();
+                    return result;
+                }
+            }
+        }
+    }
+}
